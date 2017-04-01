cb9d4bf2d912caa493acfa5d0b4f457e0ec86426
patientplatypus <pweyand@gmail.com>
2017-04-01 16:02:54 -0500

    little more

--- frontend/script.js
+++ frontend/script.js
@@ -7,53 +7,18 @@ $(function(){
 	 	var data = {
 		};
 		
-		var classreturn = $.ajax({
+		var dbretrieve = $.ajax({
 			type:"GET",
 			url:url,
 			data
 		});
 		
 
-		classreturn.done(function(response){
+		dbretrieve.done(function(err, response){
+			if (err) console.log(err);
 			console.log("YATA: ", response);
-			response.forEach(function(res){
-				$(".main").append("<li class='name' style='color:blue; background-color:red;'>"+res.name+"</li>");
-			})
 		});
 
-	//	$("body").on("click", function(){
-	//		console.log('hello')
-	//	})
-	///html/body/div/li[2]
-		//debugger;
-		$('body').on("click", ".name", function(){
-
-			console.log('This works');
-			console.log($(this).text());
-			//debugger;
-
-			
-
-			var url = 'http://localhost:3000';
-		
-			var classdelete = $.ajax({
-				type:"DELETE",
-				url:url,
-				data: {
-					name: $(this).text(),
-				}
-			});
-
- 			
-			$(this).remove();
-
-
-			classdelete.done(function(err, response){
-				//if(err) console.log("error", err);
-				console.log("DELETEYATA", response);
-
-			});
-
-		});
+	
 });
 	
