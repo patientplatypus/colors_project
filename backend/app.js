81930f767d01ab05f46bd177b3aba556c13520e0
patientplatypus <pweyand@gmail.com>
2017-04-01 11:55:19 -0500

    just starting

---
+++ backend/app.js
@@ -0,0 +1,63 @@
+require('dotenv').config();
+
+var path = require('path');
+var express = require('express');
+var cors = require('cors');
+
+
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require("mongoose");
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var classRoster = require('./routes/classRoster');
+
+var app = express();
+mongoose.connect(process.env.lab_conn_string);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
+
+app.use('/', index);
+app.use('/users', users);
+app.use('/colors', colors);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
